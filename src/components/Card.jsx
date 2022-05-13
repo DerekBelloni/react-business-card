a9f630e2266ca2ef29f485816f02045c6ea94199
Derek <derekbelloni10@gmail.com>
2022-05-13 10:49:52 -0600

    adjusted image positioning

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Photo from "./Photo";
+
+const Card = () => {
+  return (
+    <div className="card--body">
+      <Photo />
+      <div >
+        <h1>Card Template</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/Photo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import myPhoto from '../images/my-photo.jpg'
+
+const Photo = () => {
+  return (
+    <img className="card--photo" src={myPhoto} alt="" />
+  )
+}
+
+export default Photo
