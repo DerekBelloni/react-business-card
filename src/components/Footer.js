e4de9a115f11d9209018084bb70d440b50bd68ea
Derek <derekbelloni10@gmail.com>
2022-05-16 11:16:00 -0600

    brought in logos for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import gitHubLogo from '../images/github.png'
+import twitterLogo from '../images/twitter.png'
+
 const Footer = () => {
   return (
-    <h1 className="card--footer">Footer</h1>
+    <>
+      <h1 className="card--footer">Footer</h1>
+      <img className="footer--logo" src={gitHubLogo} alt="" />
+      <img className="footer--logo" src={twitterLogo} alt="" />
+      <img className="footer--logo" src={gitHubLogo} alt="" />
+      <img className="footer--logo" src={gitHubLogo} alt="" />
+    </>
   )
 }
+
 
 export default Footer;
